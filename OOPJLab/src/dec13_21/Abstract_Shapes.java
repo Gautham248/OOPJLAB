45bbe30e6a7fd8aafb318e8fb63faf707b265ab0
Student <Student@LABG035>
2021-12-20 12:39:26 +0530

    Abstract class

---
+++ OOPJLab/src/dec13_21/Abstract_Shapes.java
@@ -0,0 +1,33 @@
+package dec13_21;
+abstract class Shape{
+	abstract void numberOfSides();
+}
+ class Rectangle extends Shape{
+	 void numberOfSides() {
+		 System.out.println("I am Rectangle, i have 4 sides");
+	 }
+}
+ class Triangle extends Shape{
+	 void numberOfSides() {
+		 System.out.println("I am Triangle, i have 3 sides");
+	 }
+ }
+ class Hexagon extends Shape{
+	 void numberOfSides() {
+		 System.out.println("I am Hexagon, i have 6 sides");
+	 }
+ }
+public class Abstract_Shapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r=new Rectangle();
+		Triangle t=new Triangle();
+		Hexagon h=new Hexagon();
+		r.numberOfSides();
+		t.numberOfSides();
+		h.numberOfSides();
+		
+	}
+
+}
