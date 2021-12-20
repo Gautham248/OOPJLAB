b3f5c2fa261f2e972c8efc8a2f0048c58e20bd6f
Student <Student@LABG035>
2021-12-20 12:43:14 +0530

    to check if a string is a paliendrome

---
+++ OOPJLab/src/nov29_21/Reverse_Paliendrome.java
@@ -0,0 +1,27 @@
+package nov29_21;
+import java.util.*;
+public class Reverse_Paliendrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string:  ");
+		String str=sc.nextLine();
+		str=str.toLowerCase();
+		String revstr="";
+		char ch;
+		for(int i=0;i<str.length();i++)
+		{
+			ch=str.charAt(i);
+			revstr=ch+revstr;
+		}
+		System.out.println("Reverse of the string is:\n"+revstr);
+		if(str.equals(revstr))
+			System.out.println("The String is a Paliendrome");
+		else
+			System.out.println("The String is not a Paliendrome");
+		
+
+	}
+
+}
